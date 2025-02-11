e3577e5fbaff9fe11cd7677d62079f8d13939d3f
IbrahimVK <ibrahim.tfora@gmail.com>
2025-02-11 19:04:47 +0530

    wip

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -9,25 +9,25 @@ export const projects: Project[] = [
   {
     title: "Shakty AI",
     year: 2024,
-    description: "AI companion for your day to day life",
+    description: "Build your AI companion using your own data",
     url: "https://shakty.ai/",
   },
   {
     title: "Radr",
     year: 2023,
-    description: "A public alias for your private email",
+    description: "Privacy First: A Smart Public Alias for Secure Emailing",
     url: "https://radr.co/",
   },
   {
     title: "Ottbaba",
     year: 2025,
-    description: "One-Stop Destination for OTT Entertainment",
+    description: "One-Stop Destination for your OTT Entertainment",
     url: "https://ottbaba.com/",
   },
   {
     title: "Tallrank AI",
     year: 2024,
-    description: "AI hiring platform",
+    description: "AI-powered hiring platform",
     url: "https://tallrank.ai/",
   },
 ];
