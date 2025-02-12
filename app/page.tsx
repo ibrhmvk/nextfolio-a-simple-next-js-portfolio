7ce3623e51dce3654969d1e14826a6475ab60575
IbrahimVK <ibrahim.tfora@gmail.com>
2025-02-13 00:28:11 +0530

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
           Hi, I'm Ibrahim V K, a software developer at <a href="https://instalane.co" target="_blank">Instalane</a>, where we build AI‑powered solutions for clients. Alongside my work, I'm pursuing a <a href="https://study.iitm.ac.in/ds/" target="_blank">BS in Data Science & Applications</a> at IIT Madras.
         </p>
         <p>
-          Think of us all as either curious "monkeys" or contemplative "monks," searching for our true purpose. Seekinmonky is where that exploration happens—through constant learning, building, and experimentation. Here, I share my insights, projects, and discoveries, with a focus on math, statistics, machine learning, and more.
+          Think of us all as either curious "monkeys" or contemplative "monks," searching for our true purpose. Seekinmonky is where that exploration happens—through constant learning, building, and experimentation. Here, I share my insights, projects, and discoveries, with a focus on math, statistics, machine learning, engineering, and more.
         </p>
         <p>
           I truly believe AI will reshape our world in unimaginable ways. The best way to stay relevant is to be part of that transformation. So, explore my blog and projects—I hope you find something here that sparks your curiosity and helps you on your own journey of discovery.
