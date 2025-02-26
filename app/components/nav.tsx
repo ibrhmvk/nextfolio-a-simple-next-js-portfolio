b3bc7b6ee21d275814ac74df84fbd1c960c6b254
IbrahimVK <ibrahim.tfora@gmail.com>
2025-02-26 11:31:09 +0530

    wip

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -11,19 +11,19 @@ const navItems = {
 
 export function Navbar() {
   return (
-    <nav className="lg:mb-16 mb-8 pt-6 pb-3">
-      <div className="flex flex-col sm:flex-row sm:items-center justify-between">
+    <nav className="lg:mb-16 mb-12 py-5 pr-10">
+      <div className="flex flex-col md:flex-row md:items-center justify-between">
         <div className="flex items-center">
           <Link href="/" className="text-3xl font-semibold tracking-tight">
             {metaData.title}
           </Link>
         </div>
-        <div className="flex flex-row gap-3 sm:gap-4 mt-4 sm:mt-0 sm:ml-auto items-center overflow-x-auto pb-1 sm:pb-0">
+        <div className="flex flex-row gap-4 mt-6 md:mt-0 md:ml-auto items-center">
           {Object.entries(navItems).map(([path, { name }]) => (
             <Link
               key={path}
               href={path}
-              className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative text-sm sm:text-base py-1 px-1"
+              className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative"
             >
               {name}
             </Link>
